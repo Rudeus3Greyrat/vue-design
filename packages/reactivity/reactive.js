2e436d4eee91916669c27a42b9c33fbad4a7b373
Rudeus3Greyrat <3041084465@qq.com>
2022-02-28 11:44:15 +0800

    feat: add more track and trigger options for object

--- packages/reactivity/reactive.js
+++ packages/reactivity/reactive.js
@@ -1,8 +1,11 @@
 import { activeEffect } from './effect.js';
+import {TriggerType} from "./constant";
 // 存储所有副作用函数的大桶
 const bucket = new WeakMap();
+// for-in循环遍历的键
+const ITERATE_KEY = Symbol()
 // 触发依赖
-const trigger = (target, key) => {
+const trigger = (target, key,type) => {
   const depsMap = bucket.get(target);
   if (!depsMap) return;
   const deps = depsMap.get(key);
@@ -14,6 +17,15 @@ const trigger = (target, key) => {
       effectsToRun.add(effect);
     }
   });
+  if(type===TriggerType.ADD||type===TriggerType.DELETE){
+    const iterateDeps = depsMap.get(ITERATE_KEY)
+    // ITERATE_KEY关联的副作用函数也需要添加到effectsToRun
+    iterateDeps.forEach((effect) => {
+      if (effect !== activeEffect) {
+        effectsToRun.add(effect);
+      }
+    });
+  }
   // 执行依赖集合里的所有依赖
   effectsToRun.forEach((effectFn) => {
     // 如果传入调度器，则交由调度器执行
@@ -36,16 +48,36 @@ const track = (target, key) => {
 };
 const reactive = (data) => {
   const obj = new Proxy(data, {
-    get(target, key, receiver) {
+    get(target, p, receiver) {
       // 在读取时追踪依赖
-      track(target, key);
-      return Reflect.get(target, key, receiver);
+      track(target, p);
+      return Reflect.get(target, p, receiver);
     },
-    set(target, key, value, receiver) {
+    set(target, p, value, receiver) {
+      const type=Object.prototype.hasOwnProperty.call(target,p)?'SET':'ADD'
       // 在设置后触发依赖
-      trigger(target, key);
-      return Reflect.set(target, key, value, receiver);
+      const res=Reflect.set(target, p, value, receiver);
+      trigger(target, p,type);
+      return res;
     },
+    has(target, p) {
+      // 在调用key in obj时追踪依赖
+      track(target,p)
+      return Reflect.has(target,p)
+    },
+    ownKeys(target) {
+      // 在调用for-in循环时追踪依赖
+      track(target,ITERATE_KEY)
+      return Reflect.ownKeys(target)
+    },
+    deleteProperty(target, p) {
+      const hadKey = Object.prototype.hasOwnProperty.call(target,p)
+      const res=Reflect.deleteProperty(target,p)
+      if(res&&hadKey){
+        trigger(target,p,TriggerType.DELETE)
+      }
+      return res
+    }
   });
   return obj;
 };
